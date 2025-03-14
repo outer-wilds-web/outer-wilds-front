b6e483297291f7d5248d2936ca045b682e0bfaff
RolletQuentin <rolletquen@gmail.com>
2025-03-14 17:32:10 +0100

    change initial camera position to spawn

--- src/threejs/SceneInit.ts
+++ src/threejs/SceneInit.ts
@@ -26,7 +26,9 @@ export default class SceneInit {
       1,
       30000
     );
-    this.camera.position.z = 500;
+    // this.camera.position.z = 3000;
+    this.camera.position.x = 3050;
+
 
     this.scene = new THREE.Scene();
 
